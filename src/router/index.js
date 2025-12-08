5de122a7a1da8535d9e2c579fd7d56688bff8875
王泉 <jey@wangquandeMacBook-Pro.local>
2025-12-09 00:13:35 +0800

    fix: switch to hash router to solve github pages 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import InterviewView from '../views/InterviewView.vue'
 import ArticlesView from '../views/ArticlesView.vue'
@@ -6,7 +6,7 @@ import ArticleDetailView from '../views/ArticleDetailView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
